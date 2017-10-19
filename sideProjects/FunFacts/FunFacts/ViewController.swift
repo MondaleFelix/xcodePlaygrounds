aa4953a992e19e73142c4f964198c85e28235866
Mondale Felix <mondalefelix@yahoo.com>
2017-10-19 13:05:19 -0700

    completed practice project

--- sideProjects/FunFacts/FunFacts/ViewController.swift
+++ sideProjects/FunFacts/FunFacts/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
     let factProvider = FactProvider()
-    
+    let colorProvider = BackgroundColorProvider()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +27,9 @@ class ViewController: UIViewController {
 
     @IBAction func showFact() {
         funFactLabel.text = factProvider.randomFact()
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
     }
     
 }
