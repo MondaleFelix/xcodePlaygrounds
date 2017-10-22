65d04cfca1aa28f976a02057c82e994b1158adfe
Mondale Felix <mondalefelix@yahoo.com>
2017-10-21 22:57:26 -0700

    successfully used UIimage

---
+++ finalProject/finalProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  finalProject
+//
+//  Created by Mac on 10/21/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nerdImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.nerdImage.image = UIImage(named: "elmer")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ finalProject/finalProject/imageProvider .swift
@@ -0,0 +1,9 @@
+//
+//  imageProvider .swift
+//  finalProject
+//
+//  Created by Mac on 10/21/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import Foundation
